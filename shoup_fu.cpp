0d85d8d3f0f0a1e9fe9b93c14d6149840776d985
Dominik <oepen@informatik.hu-berlin.de>
2012-03-20 15:47:52 +0100

    - Recover the full secret AND try to compute a threshold signature - constify function arguments - Use a read only copy of the internal representation of ZZ_ps for modular exponentiation

--- shoup_fu.cpp
+++ shoup_fu.cpp
@@ -16,7 +16,7 @@ NTL_CLIENT
 
 class Share { public: int id; ZZ_p value;};
 
-ZZ_p recover_secret(Share *share) {
+ZZ_p recover_secret(Share const *share) {
     double l = 1.0;
 
 //    cout  << "Share: " << share->id << endl;
@@ -41,7 +41,7 @@ ZZ_p recover_secret(Share *share) {
     return k;
 }
 
-ZZ_p compute_threshold_sig(Share *share) {
+ZZ_p compute_threshold_sig(Share const *share) {
     double l = 1.0;
 
 //    cout  << "Share: " << share->id << endl;
@@ -62,8 +62,7 @@ ZZ_p compute_threshold_sig(Share *share) {
 //    cout << "Lagrange coefficient of Thread " << share->id << ": " << l << endl;
 
     ZZ_p k = share->value * (long) l;
-    ZZ_p m = to_ZZ_p(message);
-    ZZ_p threshold_sig = power(m, k.LoopHole());
+    ZZ_p threshold_sig = power(to_ZZ_p(message), rep(k));
 //    cout << "threshold_sig  for Thread " << share->id << ": " << threshold_sig<< endl;
 
     return threshold_sig;
@@ -82,9 +81,11 @@ int main() {
 
     /* Extract modulus and initalize NTL to use it */
     tmp = BN_bn2dec(rsa->n);
+    cout << "n = " << tmp << endl << endl;
     ZZ_p::init(to_ZZ(tmp));
     free(tmp);
     ZZ_p sig_shares[num_nodes];
+    ZZ_p sec_shares[num_nodes];
 
     /* Extract secret exponent */
     ZZ_p secret_exponent;
@@ -96,7 +97,7 @@ int main() {
     SetCoeff(poly, 0, secret_exponent);
 
 //    RSA_print_fp(stdout, rsa, 0);
-    cout << "d = " << tmp << endl;
+    cout << "d = " << tmp << endl << endl;
 //    cout << poly << endl;
 //    cout << deg(poly) << endl;
 
@@ -106,34 +107,36 @@ int main() {
         shares[i] = new Share;
         shares[i]->id = i+1;
         shares[i]->value = val;
-        sig_shares[i] = recover_secret(shares[i]);        
+        sig_shares[i] = compute_threshold_sig(shares[i]);
+        sec_shares[i] = recover_secret(shares[i]);
     }
 
-    ZZ_p combined_sig = to_ZZ_p(0);
-//    ZZ_p combined_sig = to_ZZ_p(1);
+    ZZ_p combined_sec = to_ZZ_p(0);
+    ZZ_p combined_sig = to_ZZ_p(1);
 
     /* Collect the threshold signatures from each thread and combine them */
-    for (int i=0; i<num_nodes; i++) {
-        if (i <= threshold)
-           combined_sig += sig_shares[i];   
+    for (int i=0; i<=threshold; i++) {
+        combined_sig *= sig_shares[i];
+        combined_sec += sec_shares[i];
     }
 
 //    cout << "Expected result: " << power(to_ZZ_p(message), secret_exponent.LoopHole()) << endl;
 
-    cout << "Recovered signature: " << combined_sig << endl;
-    if (combined_sig == secret_exponent)
+    cout << "Recovered secret: " << combined_sec << endl;
+/*    if (combined_sig == secret_exponent)
         cout << "Win" << endl;
     else
-        cout << "Fail" << endl;
+        cout << "Fail" << endl;*/
 
     /* Extract public exponent */
     ZZ_p public_exponent = to_ZZ_p(e);
-    ZZ_p sig = power(to_ZZ_p(message), combined_sig.LoopHole());
-    ZZ_p ver = power(sig, public_exponent.LoopHole());
-
-    /* If everything worked ver should equal message */
-    cout << "Signature: " << sig << endl << endl;
-    cout << "Verifier: " << ver << endl << endl;
+    ZZ_p sig = power(to_ZZ_p(message), rep(combined_sec));
+    ZZ_p recovered_ver = power(sig, rep(public_exponent));
+    ZZ_p ver = power(combined_sig, rep(public_exponent));
+  
+  /* If everything worked ver should equal message */
+    cout << "Recovered Verifier: " << recovered_ver << endl << endl;
+    cout << "Threshold Verifier: " << ver << endl << endl;
 
     if (rsa)
         RSA_free(rsa);
