ef8766b4963cd1cefdd1ff8f1debb8adf91e3514
Dominik <oepen@informatik.hu-berlin.de>
2012-04-05 18:23:12 +0200

    Use an RSA key with strong primes

--- Poly.cpp
+++ Poly.cpp
@@ -1,6 +1,7 @@
 #include "Poly.hh"
 #include <iostream>
 #include <openssl/rsa.h>
+#include <openssl/bn.h>
 
 #define e 65537
 #define threshold 3
@@ -194,32 +195,46 @@ void test() {
 
 int main() {
     BIGNUM *tmp = NULL;
-    BIGNUM *recovered = NULL;
-    BIGNUM *threshold_sig = NULL;
-    RSA *rsa = NULL;
+    BIGNUM *recovered = NULL, *threshold_sig = NULL;
+    BIGNUM *p = NULL, *q = NULL, *N = NULL, *e_value = NULL, *d = NULL;
     Share *shares[num_nodes];
     BIGNUM *sig_shares[num_nodes];
     BIGNUM *sec_shares[num_nodes];
-    BIGNUM *exponent = NULL;
+    BIGNUM *r0=NULL,*r1=NULL,*r2=NULL;
 
     bn_ctx = BN_CTX_new();
     BN_CTX_init(bn_ctx);
 
-    /* Generate RSA key */
-    rsa = RSA_generate_key(2048, e, NULL, NULL);
-    if (!rsa)
-        return -1;
+    /* Generate RSA key using safe primes */
+    p = BN_new();
+    BN_generate_prime_ex(p, 1024, 1, NULL, NULL, NULL);
+    q = BN_new();
+    BN_generate_prime_ex(q, 1024, 1, NULL, NULL, NULL);
+    N = BN_new();
+    BN_mul(N, p, q, bn_ctx);
+    r0 = BN_new();
+    r1 = BN_new();
+    r2 = BN_new();
+    e_value = BN_new();
+    d = BN_new();
+    BN_set_word(e_value, e);
 
-    Poly *poly = new Poly(threshold-1, rsa->n);
+    /* calculate d */
+    BN_sub(r1,p,BN_value_one());    /* p-1 */
+    BN_sub(r2,q,BN_value_one());    /* q-1 */
+    BN_mul(r0,r1,r2,bn_ctx);   /* (p-1)(q-1) */
+    BN_mod_inverse(d,e_value,N,bn_ctx);  /* d */
+
+    Poly *poly = new Poly(threshold-1, N);
 
     /* Extract secret exponent */
-    cout << BN_bn2hex(rsa->d) << endl;
-    poly->set_coeff(0, rsa->d);
+    cout << BN_bn2hex(d) << endl;
+    poly->set_coeff(0, d);
     poly->print();
 
     for (int i=0; i<num_nodes; i++) {
         tmp = poly->eval(i + 1);
-        shares[i] = new Share(i+1, tmp, rsa->n);
+        shares[i] = new Share(i+1, tmp, N);
         sig_shares[i] = compute_threshold_sig(shares[i]);
         sec_shares[i] = recover_secret(shares[i]);
     }
@@ -233,24 +248,29 @@ int main() {
     for (int i=0; i<=threshold; i++) {
 //        combined_sig *= sig_shares[i];
 //        combined_sec += sec_shares[i];
-        BN_mod_add(tmp, recovered, sec_shares[i], rsa->n, bn_ctx);
+        BN_mod_add(tmp, recovered, sec_shares[i], N, bn_ctx);
         BN_swap(tmp, recovered);
-        BN_mod_add(tmp, threshold_sig, sig_shares[i], rsa->n, bn_ctx);
+        BN_mod_add(tmp, threshold_sig, sig_shares[i], N, bn_ctx);
         BN_swap(tmp, threshold_sig);
     }
 
     cout << BN_bn2hex(recovered) << endl;
-    cout << BN_bn2hex(threshold_sig) << endl;
+//    cout << BN_bn2hex(threshold_sig) << endl;
 
-    exponent = BN_new();
-    BN_set_word(exponent, e);
-
-    BN_mod_exp(tmp, threshold_sig, exponent, rsa->n, bn_ctx);
+    BN_mod_exp(tmp, threshold_sig, e_value, N, bn_ctx);
     cout << BN_bn2dec(tmp) << endl;
     delete poly;
     BN_free(tmp);
     BN_free(recovered);
     BN_free(threshold_sig);
+    BN_free(p);
+    BN_free(q);
+    BN_free(d);
+    BN_free(e_value);
+    BN_free(N);
+    BN_free(r0);
+    BN_free(r1);
+    BN_free(r2);
     BN_CTX_free(bn_ctx);
     for (int i=0; i<num_nodes; i++) {
         delete shares[i];
